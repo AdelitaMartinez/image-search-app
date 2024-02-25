fc96625b378aed202fcc91e93a28a13cea251532
Adelita Martinez <amartinez1013@cnm.edu>
2024-02-25 08:37:30 -0700

    Added more code to script file

--- script.js
+++ script.js
@@ -21,4 +21,25 @@ async function searchImages() {
     searchResults.innerHTML = ""
   }
 
+  results.map((result) =>{
+    const imageWrapper = document.createElement('div')
+    imageWrapper.classList.add("ssearch-result")
+    const image = document.createElement('img')
+    image.src = result.urls.small
+    image.alt = result.alt_description
+    const imageLink = document.createElement('a')
+    imageLink.href = result.links.html 
+    imageLink.target = "_blank"
+    imageLink.textContent = result.alt_description
+
+    imageWrapper.appendChild(image)
+    imageWrapper.appendChild(imageLink)
+    imageWrapper.appendChild(imageWrapper)
+  })
+
+  page++
+  if(page > 1){
+    showMore.style.display = "block"
+  }
+
 }
